d49109bd66f98abaa3db2e3563c4d5b4729d232f
umerinsaan <umerinsaan@gmail.com>
2025-09-02 17:41:10 +0500

    changes for api testing

--- MI.CRM.API/Program.cs
+++ MI.CRM.API/Program.cs
@@ -17,9 +17,14 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+        //policy.WithOrigins("http://localhost:4200")
+        //      .AllowAnyHeader()
+        //      .AllowAnyMethod();
+
+        policy.AllowAnyOrigin()
+          .AllowAnyHeader()
+          .AllowAnyMethod();
+
     });
 });
 
