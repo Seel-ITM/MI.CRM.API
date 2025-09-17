94cd37a4bafc1542a995701f7d465251cb5d51c9
umerinsaan <umerinsaan@gmail.com>
2025-09-17 16:42:22 +0500

    connection string issue fixed

--- MI.CRM.API/Models/MicrmContext.cs
+++ MI.CRM.API/Models/MicrmContext.cs
@@ -46,11 +46,6 @@ public partial class MicrmContext : DbContext
     public virtual DbSet<TasksNew> TasksNews { get; set; }
 
     public virtual DbSet<User> Users { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=mi-crm-14-sep-25;Trusted_Connection=True;Encrypt=False;TrustServerCertificate=True;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<ActivityType>(entity =>
